21d482ffe1ff998f2a45da7b163287280f2012e5
Max Shen <m4xshen@gmail.com>
2026-07-13 22:08:18 +0800

    feat: update site icons and social image

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Max Shen';
-export const SITE_DESCRIPTION = 'Software engineer';
+export const SITE_DESCRIPTION = 'Product engineer from Taiwan.';
